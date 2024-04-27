ac979be25a47c9e24d4493317c44c5a3203ba12e
nuchit <jnuchit@gmail.com>
2024-04-27 16:43:03 +0700

    Add API support Graceful Shutdown

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -32,7 +36,6 @@ func main() {
 	tax.POST("/calculations", taxController.TaxCalculate)
 	tax.POST("/calculations/upload-csv", taxController.TaxCalculateFormCsv) // TODO
 
-	//  Admin BasicAuth
 	admin := e.Group("/admin")
 	admin.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
 		return username == cfg.Admin && password == cfg.AdminPassword, nil
@@ -40,15 +43,26 @@ func main() {
 
 	admin.POST("/deductions/:deductType", taxController.UpdatePersonalDeductionController)
 
-	apiPort := cfg.Port
-	if apiPort == "" {
-		apiPort = "8080"
-		log.Fatal("PORT environment variable not set")
+	apiPort := cfg.Port 
+
+	go func() {
+		if err := e.Start(":" + apiPort); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal(e.Start(":" + apiPort))
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+
+	// Print "shutting down the server"
+	fmt.Println("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
 	}
 
-	address := fmt.Sprintf(":%s", apiPort)
-	fmt.Printf("Server is running on port %s\n", apiPort)
-	if err := e.Start(address); err != nil {
-		log.Fatal("Error starting server: ", err)
-	}
 }
